8242cb4d7abf774bb9b2c1902aa0b5e5149075c2
csdoker <csd758371536@qq.com>
2020-01-01 23:17:05 +0800

    搭建页面结构

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="sidebar-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Siderbar'
+}
+</script>
---
+++ src/layout/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-wrapper">
+    <sidebar />
+    <app-main />
+  </div>
+</template>
+
+<script>
+import { Sidebar, AppMain } from './components'
+
+export default {
+  name: 'Layout',
+  components: {
+    Sidebar,
+    AppMain
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .app-wrapper {
+
+  }
+</style>
