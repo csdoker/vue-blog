1d773a189e2142a9700e0f08cd4bd0caac54d4f6
csdoker <csd758371536@qq.com>
2020-01-06 15:40:00 +0800

    引入nprogress插件，完成进度条效果

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+
+import getPageTitle from '@/utils/get-page-title'
+
 /* Layout */
 import Layout from '@/layout'
+
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 Vue.use(Router)
 
@@ -47,8 +54,33 @@ const routes = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
-const router = new Router({
-  routes
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: routes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter () {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+router.beforeEach((to, from, next) => {
+  // start progress bar
+  NProgress.start()
+
+  // set page title
+  document.title = getPageTitle(to.meta.title)
+
+  next()
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
 })
 
 export default router
