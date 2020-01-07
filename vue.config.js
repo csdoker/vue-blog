cb664fd68474a61a6900a2757921fb69961ab653
csdoker <csd758371536@qq.com>
2020-01-07 15:17:06 +0800

    引入vue-markdown-loader插件，并添加相关配置

--- vue.config.js
+++ vue.config.js
@@ -43,5 +43,17 @@ module.exports = {
         '@': resolve('src')
       }
     }
+  },
+  chainWebpack: config => {
+    config.module.rule('md')
+      .test(/\.md/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-markdown-loader')
+      .loader('vue-markdown-loader/lib/markdown-compiler')
+      .options({
+        raw: true
+      })
   }
 }
