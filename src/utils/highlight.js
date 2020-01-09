f378b92ce10dec277faa2f82aaa0a80bdef27139
csdoker <csd758371536@qq.com>
2020-01-09 21:09:32 +0800

    优化highlight引入方式，解决打包后文件过大的问题

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -1,7 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import Vue from 'vue'
-import hljs from 'highlight.js'
+import hljs from 'highlight.js/lib/highlight'
 import 'highlight.js/styles/atom-one-dark.css'
+
+// 只加载常用语法
+import javascript from 'highlight.js/lib/languages/javascript'
+import typescript from 'highlight.js/lib/languages/typescript'
+import xml from 'highlight.js/lib/languages/xml'
+import css from 'highlight.js/lib/languages/css'
+import scss from 'highlight.js/lib/languages/scss'
+import markdown from 'highlight.js/lib/languages/markdown'
+
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('typescript', typescript)
+hljs.registerLanguage('xml', xml)
+hljs.registerLanguage('css', css)
+hljs.registerLanguage('scss', scss)
+hljs.registerLanguage('markdown', markdown)
 
 // const Highlight = {}
 // Highlight.install = function (Vue, options) {
