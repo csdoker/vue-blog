d136f0d0c2f804ad58206b9b9c5821e65cae947e
csdoker <csd758371536@qq.com>
2020-01-09 16:04:43 +0800

    完成归档页文章数据的分割与排序

---
+++ src/views/archive/archive-list/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="archive-list">
+  </div>
+</template>
+
+<script>
+import BLOGENTRIES from '@/data/blogs.json'
+
+export default {
+  name: 'ArchiveList',
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    maxCount: {
+      type: Number,
+      default: 10
+    }
+  },
+  computed: {
+    articles () {
+      const start = (this.currentPage - 1) * this.maxCount
+      const end = start + this.maxCount
+      return BLOGENTRIES.sort((a, b) => b.id - a.id).slice(start, end)
+    },
+    archives () {
+      let chunkResult = this.chunkArr(this.articles, this.maxCount)
+      let result = []
+      for (let i = 0; i < chunkResult.length; i++) {
+        // 抽离
+        let _datas = this.sortArr(chunkResult[i], 'date')
+        // 根据所给数据结构进行赋值
+        result.push([])
+        for (let o = 0; o < _datas.length; o++) {
+          result[i].push({
+            date: new Date(_datas[o][0].date).getFullYear(),
+            articles: []
+          })
+        }
+        for (let p = 0; p < result[i].length; p++) {
+          result[i][p].articles = this.sortArr(chunkResult[i], 'date')[p]
+        }
+      }
+      return result
+    }
+  },
+  methods: {
+    chunkArr (array, size) {
+      // 获取数组的长度，如果你传入的不是数组，那么获取到的就是undefined
+      const length = array.length
+      // 判断不是数组，或者size没有设置，size小于1，就返回空数组
+      if (!length || !size || size < 1) {
+        return []
+      }
+      // 用来表示切割元素的范围start
+      let index = 0
+      // 用来递增表示输出数组的下标
+      let resIndex = 0
+      // 根据length和size算出输出数组的长度，并且创建它。
+      let result = new Array(Math.ceil(length / size))
+      // 进行循环
+      while (index < length) {
+        // 循环过程中设置result[0]和result[1]的值。该值根据array.slice切割得到。
+        result[resIndex++] = array.slice(index, (index += size))
+      }
+      // 输出新数组
+      // return result
+      return result
+    },
+    sortArr (array, prop) {
+      let result = []
+      let entry = []
+      let temp = 0
+      // 按照特定的参数将数组排序将具有相同值得排在一起
+      array = array.sort((a, b) => {
+        return a[prop] > b[prop] ? -1 : 1
+      })
+      if (array.length) {
+        temp = new Date(array[0][prop]).getFullYear()
+      }
+      // 将相同类别的对象添加到统一个数组
+      for (let i = 0; i < array.length; i++) {
+        if (new Date(array[i][prop]).getFullYear() === temp) {
+          entry.push(array[i])
+        } else {
+          temp = new Date(array[i][prop]).getFullYear()
+          result.push(entry)
+          entry = [array[i]]
+        }
+      }
+      // 将最后的内容推出新数组
+      result.push(entry)
+      return result
+    }
+  },
+  created () {
+    console.log(this.archives)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/archive/index.vue
+++ src/views/archive/index.vue
@@ -1,11 +1,47 @@
 <template>
   <div class="archive-container">
-
+    <archive-list :currentPage="currentPage" :maxCount="maxCount" />
+    <pager :hide-if-one-page="false" :total-page="pageCount" :current-page.sync="currentPage" @update:currentPage="updatePage" />
   </div>
 </template>
 
 <script>
+import ArchiveList from './archive-list'
+import Pager from '@/components/Pager'
+import BLOGENTRIES from '@/data/blogs.json'
+
 export default {
-  name: 'Archive'
+  name: 'Archive',
+  components: {
+    ArchiveList,
+    Pager
+  },
+  data () {
+    return {
+      currentPage: 1,
+      maxCount: 10,
+      totalCount: BLOGENTRIES.length
+    }
+  },
+  computed: {
+    pageCount () {
+      let pageCount = 0
+      if (this.totalCount % this.maxCount === 0) {
+        pageCount = this.totalCount / this.maxCount
+      } else {
+        pageCount = (this.totalCount - this.totalCount % this.maxCount) / this.maxCount + 1
+      }
+      return pageCount
+    }
+  },
+  methods: {
+    updatePage (page) {
+      this.currentPage = page
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+
+</style>
